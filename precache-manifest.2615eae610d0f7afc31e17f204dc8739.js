c543a04f7c6c6ac824ba8d768d3429207ebe2a05
TuTran21 <tmtwns@gmail.com>
2020-07-20 20:59:41 +0700

    Updates

---
+++ precache-manifest.2615eae610d0f7afc31e17f204dc8739.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0790576bdfcd0714f80a9b297ad90d79",
+    "url": "/Tu-Tran-Portfolio/index.html"
+  },
+  {
+    "revision": "4326fc70367600527149",
+    "url": "/Tu-Tran-Portfolio/static/js/2.01efa24e.chunk.js"
+  },
+  {
+    "revision": "ea25f1fd7eb39c41dd4f",
+    "url": "/Tu-Tran-Portfolio/static/js/main.c335229b.chunk.js"
+  },
+  {
+    "revision": "eecb4310d8d63da58984",
+    "url": "/Tu-Tran-Portfolio/static/js/runtime~main.1bf6a47d.js"
+  }
+]);
